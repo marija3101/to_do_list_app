c07f4797658dd5b1f98856a2fa1d023580c15aa9
Marija Martinovic <marijaamartinovicc11@gmail.com>
2024-09-21 17:31:36 +0200

    Kreirana Vue aplikacija unutar Laravela

--- to-do-list-app/resources/js/app.js
+++ to-do-list-app/resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import { createApp } from "vue";
+import TaskComponent from "./components/TaskComponent.vue";
+
+const app = createApp({});
+app.component("task-component", TaskComponent);
+app.mount("#app");
